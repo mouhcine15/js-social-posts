365dfcd79feac3f2ff5d9191c1878fbffc8d3215
mouhcine <mouhcine15.00@gmail.com>
2022-05-20 16:13:49 +0200

    stampa post

--- main.js
+++ main.js
@@ -55,3 +55,48 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const container = document.querySelector('#container');
+
+
+function stampa(){
+    
+    let item = '';
+    
+    for(let i = 0; i < posts.length; i++ ){
+        item +=   ` <div class="post">
+            <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${posts[i].author.image}" alt="${posts[i].author.name} ">                   
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author"> ${posts[i].author.name}</div>
+                    <div class="post-meta__time"> ${posts[i].created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text"> ${posts[i].content}</div>
+        <div class="post__image">
+            <img ${posts[i].media}
+            src=" ${posts[i].media}" alt="Immagine post di ${posts[i].author.name}">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="${posts[i].id}">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+    }
+    container.innerHTML += item;
+};
+
+stampa();
